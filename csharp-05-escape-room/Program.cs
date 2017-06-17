7276c916b620709125476455e8844c72a631132c
Logan Kelly <logan.r.kelly@gmail.com>
2017-06-16 20:02:06 -0500

    Room class created

---
+++ csharp-05-escape-room/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace csharp05escaperoom
+{
+    public class Room
+    {
+        private string rName;
+        private string rDescription;
+
+        public string RoomName
+        {
+            get
+            {
+                return rName;
+            }
+            set
+            {
+                rName = value;
+            }
+        }
+
+        public string RoomDescription
+        {
+            get
+            {
+                return rDescription;
+            }
+            set
+            {
+                rDescription = value;
+            }
+        }
+
+        public void PrintRoomName()
+        {
+            Console.WriteLine(rName);
+            for (int i = 0; i < rName.Length; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+        }
+
+        public void PrintRoomDescription()
+        {
+            Console.WriteLine(rDescription);
+        }
+
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
