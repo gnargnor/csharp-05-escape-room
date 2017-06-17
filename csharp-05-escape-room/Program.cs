30767d6127c758c8e18a9aa3b5ea3dc2802dfc75
Logan Kelly <logan.r.kelly@gmail.com>
2017-06-17 12:47:54 -0500

    my first class - a computer room.  of course.

--- csharp-05-escape-room/Program.cs
+++ csharp-05-escape-room/Program.cs
@@ -46,13 +46,26 @@ namespace csharp05escaperoom
             Console.WriteLine(rDescription);
         }
 
+        public override string ToString()
+        {
+            return "Room Name: " + rName + ", Room Description " + rDescription;
+        }
+
+        public Room(string roomName, string roomDescription)
+        {
+            rName = roomName;
+            rDescription = roomDescription;
+            PrintRoomName();
+            PrintRoomDescription();
+        }
+
     }
 
     class MainClass
     {
         public static void Main(string[] args)
         {
-            
+            Room computerRoom = new Room("Computer Room", "You find yourself in the basement in the Computer Room.  It smells like sweat and frustration.  You see a Macbook on a plain white desk surrounded by b.. soda cans.  There are two monitors flanking the Macbook, predictably.  Logan is sleeping on the floor.");
         }
     }
 }
