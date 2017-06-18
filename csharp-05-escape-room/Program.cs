fe09dba4a9c8bb00ee2175b397b36c4828c82a8f
Logan Kelly <logan.r.kelly@gmail.com>
2017-06-18 17:18:32 -0500

    hugged my brackets

--- csharp-05-escape-room/Program.cs
+++ csharp-05-escape-room/Program.cs
@@ -2,69 +2,54 @@
 
 namespace csharp05escaperoom
 {
-    public class Room
-    {
+    public class Room {
         private string rName;
         private string rDescription;
 
-        public string RoomName
-        {
-            get
-            {
+        public string RoomName {
+            get {
                 return rName;
             }
-            set
-            {
+            set {
                 rName = value;
             }
         }
 
-        public string RoomDescription
-        {
-            get
-            {
+        public string RoomDescription {
+            get {
                 return rDescription;
             }
-            set
-            {
+            set {
                 rDescription = value;
             }
         }
 
-        public void PrintRoomName()
-        {
+        public void PrintRoomName() {
             Console.WriteLine(rName);
-            for (int i = 0; i < rName.Length; i++)
-            {
+            for (int i = 0; i < rName.Length; i++) {
                 Console.Write("-");
             }
             Console.WriteLine();
         }
 
-        public void PrintRoomDescription()
-        {
+        public void PrintRoomDescription() {
             Console.WriteLine(rDescription);
         }
 
-        public override string ToString()
-        {
+        public override string ToString() {
             return "Room Name: " + rName + ", Room Description " + rDescription;
         }
 
-        public Room(string roomName, string roomDescription)
-        {
+        public Room(string roomName, string roomDescription) {
             rName = roomName;
             rDescription = roomDescription;
             PrintRoomName();
             PrintRoomDescription();
         }
-
     }
 
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
+    class MainClass {
+        public static void Main(string[] args) {
             Room computerRoom = new Room("Computer Room", "You find yourself in the basement in the Computer Room.  It smells like sweat and frustration.  You see a Macbook on a plain white desk surrounded by b.. soda cans.  There are two monitors flanking the Macbook, predictably.  Logan is sleeping on the floor.");
         }
     }
